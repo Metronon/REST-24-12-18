19c538f44af23984a6a6a40c733fd26068973174
Metronon <bumpercar07@gmail.com>
2024-12-18 15:21:01 +0900

    HTTP method implement

--- src/main/java/com/ll/rest/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/ll/rest/domain/post/post/controller/ApiV1PostController.java
@@ -2,12 +2,15 @@ package com.ll.rest.domain.post.post.controller;
 
 import com.ll.rest.domain.post.post.entity.Post;
 import com.ll.rest.domain.post.post.service.PostService;
+import com.ll.rest.global.rsData.RsData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/posts")
@@ -20,6 +23,7 @@ public class ApiV1PostController {
         return postService.findAllByOrderByIdDesc();
     }
 
+
     @GetMapping("/{id}")
     public Post getItem(
             @PathVariable long id
@@ -27,18 +31,68 @@ public class ApiV1PostController {
         return postService.findById(id).get();
     }
 
+
     @DeleteMapping("/{id}")
-    public Map<String, Object> deleteItem(
+    public RsData deleteItem(
             @PathVariable long id
     ) {
         Post post = postService.findById(id).get();
 
         postService.delete(post);
 
-        Map<String, Object> rsData = new HashMap<>();
-        rsData.put("resultCode", "200-1");
-        rsData.put("msg", "%d번 글을 삭제하였습니다.".formatted(id));
+        return new RsData(
+                "200-1",
+                "%d번 글을 삭제하였습니다.".formatted(id)
+        );
+    }
 
-        return rsData;
+
+    record PostModifyReqBody(
+            @NotBlank
+            @Length(min = 2)
+            String title,
+            @NotBlank
+            @Length(min = 2)
+            String content
+    ) {
+    }
+
+    @PutMapping("/{id}")
+    @Transactional
+    public RsData modifyItem(
+            @PathVariable long id,
+            @RequestBody @Valid PostModifyReqBody reqBody
+    ) {
+        Post post = postService.findById(id).get();
+
+        postService.modify(post, reqBody.title, reqBody.content);
+
+        return new RsData(
+                "200-1",
+                "%d번 글이 수정되었습니다.".formatted(id)
+        );
+    }
+
+
+    record PostWriteReqBody(
+            @NotBlank
+            @Length(min = 2)
+            String title,
+            @NotBlank
+            @Length(min = 2)
+            String content
+    ) {
+    }
+
+    @PostMapping
+    public RsData writeItem(
+            @RequestBody @Valid PostWriteReqBody reqBody
+    ) {
+        Post post = postService.write(reqBody.title, reqBody.content);
+
+        return new RsData(
+                "200-1",
+                "%d번 글이 작성되었습니다.".formatted(post.getId())
+        );
     }
 }
