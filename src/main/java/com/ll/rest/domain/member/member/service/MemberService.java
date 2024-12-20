1dfa0f2876cadfa7428054dcf81b32321a1f3a06
Metronon <bumpercar07@gmail.com>
2024-12-20 14:03:33 +0900

    member package & add exception handler

---
+++ src/main/java/com/ll/rest/domain/member/member/service/MemberService.java
@@ -0,0 +1,33 @@
+package com.ll.rest.domain.member.member.service;
+
+import com.ll.rest.domain.member.member.entity.Member;
+import com.ll.rest.domain.member.member.repository.MemberRepository;
+import com.ll.rest.global.exceptions.ServiceException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public long count() {
+        return memberRepository.count();
+    }
+
+    public Member join(String username, String password, String nickname) {
+        memberRepository
+                .findByUsername(username)
+                .ifPresent(existingMember -> {
+                    throw new ServiceException("400-1", "해당 아이디는 이미 사용중입니다.");
+                });
+
+        Member member = Member.builder()
+                .username(username)
+                .password(password)
+                .nickname(nickname)
+                .build();
+
+        return memberRepository.save(member);
+    }
+}
